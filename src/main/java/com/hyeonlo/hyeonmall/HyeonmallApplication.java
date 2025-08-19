de6ce98f57917746bb16f51d539b5011775ca3f9
ks12467 <ks12467@naver.com>
2025-08-19 22:02:34 +0900

    first commit

---
+++ src/main/java/com/hyeonlo/hyeonmall/HyeonmallApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonlo.hyeonmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeonmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyeonmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeonlo/hyeonmall/HyeonmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonlo.hyeonmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyeonmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
